84fc5c4a3ae14bd4462b9848d149be909c67ace8
Wellington Silva <wellingtontsb@gmail.com>
2024-11-20 18:33:32 -0300

    refactoring and adding javascript animation on banner section

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+let totalSlides = document.querySelectorAll('.slide').length;
+let slideArea = document.querySelector('.container').clientWidth;
+let dotsSlide = document.querySelector('.banner-points').children;
+let dots = document.querySelectorAll('.point');
+let currentSlide = 0;
+
+//calcula o tamanho do width automaticaticamente
+document.querySelector('.sliders').style.width = `calc(${slideArea} * ${totalSlides})`;
+
+//insere o tamanho da altura dos botoes next e prev automaticamente
+document.querySelector('.slider--controls').style.height = `${document.querySelector('.banner-slide').clientHeight}px`;
+
+function goPrev() {
+	dotsSlide[currentSlide].classList.remove('active');
+	currentSlide--;
+	if (currentSlide < 0) {
+		currentSlide = totalSlides - 1;
+	}
+	dotsSlide[currentSlide].classList.add('active');
+	updateMargin();
+}
+
+function goNext() {
+	dotsSlide[currentSlide].classList.remove('active');
+	currentSlide++;
+	if (currentSlide > (totalSlides - 1)) {
+		currentSlide = 0;
+	}
+	dotsSlide[currentSlide].classList.add('active');
+	updateMargin();
+}
+
+function updateMargin() {
+	let sliderItemWidth = document.querySelector('.slide').clientWidth;
+	let newMargin = currentSlide * sliderItemWidth;
+	document.querySelector('.slide').style.marginLeft = `-${newMargin}px`;
+}
+
+dots.forEach((item, index)=>{
+	item.addEventListener('click', ()=>{
+		dotsSlide[currentSlide].classList.remove('active');
+		item.classList.add('active');
+		currentSlide = index;
+		updateMargin();
+	});
+});
+
+//funcao para mudar os slides automaticamente
+
+/*
+setInterval(()=>{
+	updateMargin();
+	goNext();
+}, 5000);
+*/
