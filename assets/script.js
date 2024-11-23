fba59611a11bb826185023b7aaab0aabb4635463
Wellington Silva <wellingtontsb@gmail.com>
2024-11-22 22:09:40 -0300

    refactoring and adding javascript on offers section

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//CODIGO PARA O FUNCIONAMENTO DOS SLIDES DA BANNER SECTION
 let totalSlides = document.querySelectorAll('.slide').length;
 let slideArea = document.querySelector('.container').clientWidth;
 let dotsSlide = document.querySelector('.banner-points').children;
@@ -8,7 +9,8 @@ let currentSlide = 0;
 document.querySelector('.sliders').style.width = `calc(${slideArea} * ${totalSlides})`;
 
 //insere o tamanho da altura dos botoes next e prev automaticamente
-document.querySelector('.slider--controls').style.height = `${document.querySelector('.banner-slide').clientHeight}px`;
+document.querySelector('.slider-banner-controls').style.height = `${document.querySelector('.banner-slide').clientHeight}px`;
+//document.querySelector('.slider-offers-controls').style.height = `${document.querySelector('.offers').clientHeight}px`;
 
 function goPrev() {
 	dotsSlide[currentSlide].classList.remove('active');
@@ -53,3 +55,32 @@ setInterval(()=>{
 	goNext();
 }, 5000);
 */
+
+
+//CODIGO PARA O FUNCIONAMENTO DOS SLIDES DA OFFERS SECTION
+let offersSlide = document.querySelectorAll('.offers-slide').length;
+let currentOffersSlide = 0;
+
+document.querySelector('.offers-sliders').style.width = `calc(${slideArea} * ${offersSlide})px`;
+
+function goPrevOffers(){
+	currentOffersSlide--;
+	if (currentOffersSlide < 0) {
+		currentOffersSlide = offersSlide - 1;
+	}
+	updateMarginOffers();
+}
+
+function goNextOffers(){
+	currentOffersSlide++;
+	if (currentOffersSlide > (offersSlide - 1)) {
+		currentOffersSlide = 0;
+	}
+	updateMarginOffers();
+}
+
+function updateMarginOffers(){
+	let offerSlideWidth = document.querySelector('.offers-slide').clientWidth;
+	let newMarginOffer = currentOffersSlide * (offerSlideWidth + 36);
+	document.querySelector('.offers-slide').style.marginLeft = `-${newMarginOffer}px`;
+}
